5ef4f013a9a876348eb9ae84c7938d0b0a0ba8e1
2IMT <daniil_t04@mail.ru>
2026-04-05 22:42:38 +0300

    Update default config

--- src/config.h
+++ src/config.h
@@ -5,12 +5,11 @@
 
 #define SHCMD(cmd_) { "/bin/sh", "-c", cmd_, NULL }
 
-static const char* volume_cmd[] = 
-    SHCMD("pactl get-sink-volume @DEFAULT_SINK@ | awk 'NR==1{print $5}'");
 static const char* layout_cmd[] = { "xkb-switch", NULL };
 
 #include "modules/temperature.h"
 #include "modules/cpu.h"
+#include "modules/volume.h"
 #include "modules/customcmd.h"
 #include "modules/time.h"
 #include "modules/battery.h"
@@ -20,7 +19,7 @@ static const Module mod_temperature =
 static const Module mod_cpu = 
     MODULE(Cpu, DEFAULTS);
 static const Module mod_volume = 
-    MODULE(CustomCmd, .command = volume_cmd);
+    MODULE(Volume, .muted_text = "x");
 static const Module mod_layout = 
     MODULE(CustomCmd, .command = layout_cmd);
 static const Module mod_time = 
@@ -29,12 +28,12 @@ static const Module mod_battery =
     MODULE(Battery, .battery_name = "BAT1", .adapter_name = "ACAD");
 
 static const Format status_format[] = {
-    FTEXT("[temp:"), FMOD(mod_temperature), FTEXT("C] "),
-    FTEXT("[cpu:"), FMOD(mod_cpu), FTEXT("%] "),
-    FTEXT("[vol:"), FMOD(mod_volume), FTEXT("] "),
-    FTEXT("["), FMOD(mod_layout), FTEXT("] "),
-    FTEXT("["), FMOD(mod_time), FTEXT("] "),
-    FTEXT("[bat:"), FMOD(mod_battery), FTEXT("]"),
+    FTEXT("[t:"), FMOD(mod_temperature), FTEXT("C]"),
+    FTEXT("[c:"), FMOD(mod_cpu), FTEXT("%]"),
+    FTEXT("[v:"), FMOD(mod_volume), FTEXT("]"),
+    FTEXT("["), FMOD(mod_layout), FTEXT("]"),
+    FTEXT("["), FMOD(mod_time), FTEXT("]"),
+    FTEXT("[b:"), FMOD(mod_battery), FTEXT("]"),
 };
 
 static const double status_update_interval_seconds = 1.0;
